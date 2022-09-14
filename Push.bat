7a8c519b632f82af217b9d56ca9c936562b0d64c
CIT142F-10@M307.UC-BCF.EDU.PH <CIT142F-10@M307.UC-BCF.EDU.PH>
2022-09-14 17:24:12 +0800

    batch upload

--- Push.bat
+++ Push.bat
@@ -1,6 +1,6 @@
 @echo off
 git remote set-url origin https://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@github.com/synnejim/synne.git
+pause
 git add .
 git commit -m "batch upload"
 git push 
-pause 
